8dbcfff81c7583c0ebd9520d22c80437c6254147
zafarimam <md.imam@microsoft.com>
2017-06-11 14:47:44 +0530

    update

---
+++ WebApplication111/WebApplication111/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication111.MvcApplication" Language="C#" %>
---
+++ WebApplication111/WebApplication111/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication111.Startup))]
+namespace WebApplication111
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
